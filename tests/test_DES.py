88c41f2c3118d47f1a0403e0c80d37d7d4c0b8d5
GitHub <noreply@github.com>
2020-05-09 23:40:07 +0300

    Added tests folder

---
+++ tests/test_DES.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_left_rotate(self):
+        self.fail()
+
+    def test_permute(self):
+        self.fail()
+
+    def test_gen_subkeys(self):
+        self.fail()
+
+    def test_xor(self):
+        self.fail()
+
+    def test_s_box(self):
+        self.fail()
+
+    def test_read(self):
+        self.fail()
+
+    def test_pad(self):
+        self.fail()
+
+    def test_round(self):
+        self.fail()
+
+    def test_des(self):
+        self.fail()
+
+    def test_crypt(self):
+        self.fail()
+
+    def test_menu(self):
+        self.fail()
