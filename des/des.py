4d949a79b5f8edc5f38be13aa47c695f733fa21a
Vipul Gharde <vipul.gharde@gmail.com>
2024-10-24 03:03:04 -0400

    Update code

--- des/des.py
+++ des/des.py
@@ -4,21 +4,136 @@ import argparse
 
 BLOCK_SIZE = 64
 
+KEY_PERMUTATION_TABLE = [
+    56, 48, 40, 32, 24, 16, 8,
+    0, 57, 49, 41, 33, 25, 17,
+    9, 1, 58, 50, 42, 34, 26,
+    18, 10, 2, 59, 51, 43, 35,
+    62, 54, 46, 38, 30, 22, 14,
+    6, 61, 53, 45, 37, 29, 21,
+    13, 5, 60, 52, 44, 36, 28,
+    20, 12, 4, 27, 19, 11, 3
+]
+
+COMPRESSION_PERMUTATION_TABLE = [
+    13, 16, 10, 23, 0, 4,
+    2, 27, 14, 5, 20, 9,
+    22, 18, 11, 3, 25, 7,
+    15, 6, 26, 19, 12, 1,
+    40, 51, 30, 36, 46, 54,
+    29, 39, 50, 44, 32, 47,
+    43, 48, 38, 55, 33, 52,
+    45, 41, 49, 35, 28, 31
+]
+
+S_BOX_TABLE = [
+    [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+        [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+        [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+    ],
+    [
+        [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+        [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+        [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+        [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+    ],
+    [
+        [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+        [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+        [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+        [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+    ],
+    [
+        [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+        [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+        [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+        [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+    ],
+    [
+        [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+        [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+        [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+        [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+    ],
+    [
+        [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+        [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+        [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+        [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+    ],
+    [
+        [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+        [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+        [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+        [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+    ],
+    [
+        [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+        [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+        [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+        [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+    ]
+]
+
+EXPANSION_PERMUTATION_TABLE = [
+    31, 0, 1, 2, 3, 4,
+    3, 4, 5, 6, 7, 8,
+    7, 8, 9, 10, 11, 12,
+    11, 12, 13, 14, 15, 16,
+    15, 16, 17, 18, 19, 20,
+    19, 20, 21, 22, 23, 24,
+    23, 24, 25, 26, 27, 28,
+    27, 28, 29, 30, 31, 0
+]
+
+P_BOX_TABLE = [
+    15, 6, 19, 20, 28, 11, 27, 16,
+    0, 14, 22, 25, 4, 17, 30, 9,
+    1, 7, 23, 13, 31, 26, 2, 8,
+    18, 12, 29, 5, 21, 10, 3, 24
+]
+
+INITIAL_PERMUTATION_TABLE = [
+    57, 49, 41, 33, 25, 17, 9, 1,
+    59, 51, 43, 35, 27, 19, 11, 3,
+    61, 53, 45, 37, 29, 21, 13, 5,
+    63, 55, 47, 39, 31, 23, 15, 7,
+    56, 48, 40, 32, 24, 16, 8, 0,
+    58, 50, 42, 34, 26, 18, 10, 2,
+    60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6
+]
+
+FINAL_PERMUTATION_TABLE = [
+    39, 7, 47, 15, 55, 23, 63, 31,
+    38, 6, 46, 14, 54, 22, 62, 30,
+    37, 5, 45, 13, 53, 21, 61, 29,
+    36, 4, 44, 12, 52, 20, 60, 28,
+    35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26,
+    33, 1, 41, 9, 49, 17, 57, 25,
+    32, 0, 40, 8, 48, 16, 56, 24
+]
+
 
 def hex_to_bin(hex_str):
     return f'{int(hex_str, 16):0{len(4 * hex_str)}b}'
 
 
 def pad(bin_str):
-    return bin_str + '0' * ((BLOCK_SIZE - len(bin_str) % BLOCK_SIZE) % BLOCK_SIZE)
+    padding_length = (BLOCK_SIZE - len(bin_str) % BLOCK_SIZE) % BLOCK_SIZE
+    return bin_str + '0' * padding_length
 
 
-def fprint(text, variable):
-    print(f'{text:>22}: {variable}')
+def fprint(text, value):
+    print(f'{text:>22}: {value}')
 
 
 def split_block(block):
-    return block[:len(block) // 2], block[len(block) // 2:]
+    mid = len(block) // 2
+    return block[:mid], block[mid:]
 
 
 def left_rotate(blocks, n_shifts):
@@ -30,39 +145,18 @@ def permute(block, table):
 
 
 def gen_subkeys(key):
-    key_permutation_table = [
-        56, 48, 40, 32, 24, 16, 8,
-        0, 57, 49, 41, 33, 25, 17,
-        9, 1, 58, 50, 42, 34, 26,
-        18, 10, 2, 59, 51, 43, 35,
-        62, 54, 46, 38, 30, 22, 14,
-        6, 61, 53, 45, 37, 29, 21,
-        13, 5, 60, 52, 44, 36, 28,
-        20, 12, 4, 27, 19, 11, 3
-    ]
-    compression_permutation_table = [
-        13, 16, 10, 23, 0, 4,
-        2, 27, 14, 5, 20, 9,
-        22, 18, 11, 3, 25, 7,
-        15, 6, 26, 19, 12, 1,
-        40, 51, 30, 36, 46, 54,
-        29, 39, 50, 44, 32, 47,
-        43, 48, 38, 55, 33, 52,
-        45, 41, 49, 35, 28, 31
-    ]
     left_rotate_order = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
-
-    key_permutation = permute(key, key_permutation_table)
+    key_permutation = permute(key, KEY_PERMUTATION_TABLE)
 
     fprint('KEY', key)
     fprint('KEY PERMUTATION', key_permutation)
 
     lk, rk = split_block(key_permutation)
-
     subkeys = []
+
     for n_shifts in left_rotate_order:
         lk, rk = left_rotate([lk, rk], n_shifts)
-        compression_permutation = permute(lk + rk, compression_permutation_table)
+        compression_permutation = permute(lk + rk, COMPRESSION_PERMUTATION_TABLE)
         subkeys.append(compression_permutation)
 
     return subkeys
@@ -73,152 +167,55 @@ def xor(block_1, block_2):
 
 
 def s_box(block):
-    s_box_table = [
-        [
-            [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-            [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
-            [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
-        ],
-        [
-            [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
-            [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
-            [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
-            [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
-        ],
-        [
-            [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
-            [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
-            [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
-            [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
-        ],
-        [
-            [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
-            [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
-            [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
-            [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
-        ],
-        [
-            [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
-            [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
-            [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
-            [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
-        ],
-        [
-            [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
-            [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
-            [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
-            [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
-        ],
-        [
-            [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
-            [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
-            [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
-            [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
-        ],
-        [
-            [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
-            [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
-            [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-            [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
-        ]
-    ]
     output = ''
-
     for i in range(8):
         sub_str = block[i * 6:i * 6 + 6]
         row = int(sub_str[0] + sub_str[-1], 2)
         column = int(sub_str[1:5], 2)
-        output += f'{s_box_table[i][row][column]:04b}'
-
+        output += f'{S_BOX_TABLE[i][row][column]:04b}'
     return output
 
 
 def round(input_block, subkey):
-    expansion_permutation_table = [
-        31, 0, 1, 2, 3, 4,
-        3, 4, 5, 6, 7, 8,
-        7, 8, 9, 10, 11, 12,
-        11, 12, 13, 14, 15, 16,
-        15, 16, 17, 18, 19, 20,
-        19, 20, 21, 22, 23, 24,
-        23, 24, 25, 26, 27, 28,
-        27, 28, 29, 30, 31, 0
-    ]
-    p_box_table = [
-        15, 6, 19, 20, 28, 11, 27, 16,
-        0, 14, 22, 25, 4, 17, 30, 9,
-        1, 7, 23, 13, 31, 26, 2, 8,
-        18, 12, 29, 5, 21, 10, 3, 24
-    ]
-
     l, r = split_block(input_block)
-    expansion_permutation = permute(r, expansion_permutation_table)
-    xor_1 = xor(expansion_permutation, subkey)
-    s_box_output = s_box(xor_1)
-    p_box = permute(s_box_output, p_box_table)
-    xor_2 = xor(p_box, l)
-    output = r + xor_2
+    expansion_permutation = permute(r, EXPANSION_PERMUTATION_TABLE)
+    xor_with_subkey = xor(expansion_permutation, subkey)
+    s_box_output = s_box(xor_with_subkey)
+    p_box_output = permute(s_box_output, P_BOX_TABLE)
+    xor_with_left = xor(p_box_output, l)
+    output = r + xor_with_left
 
     fprint('INPUT', f'{l} {r}')
     fprint('SUBKEY', subkey)
     fprint('EXPANSION PERMUTATION', expansion_permutation)
-    fprint('XOR', xor_1)
+    fprint('XOR', xor_with_subkey)
     fprint('S-BOX SUBSTITUTION', s_box_output)
-    fprint('P-BOX PERMUTATION', p_box)
-    fprint('XOR', xor_2)
-    fprint('SWAP', f'{r} {xor_2}')
+    fprint('P-BOX PERMUTATION', p_box_output)
+    fprint('XOR', xor_with_left)
+    fprint('SWAP', f'{r} {xor_with_left}')
     fprint('OUTPUT', output)
 
     return output
 
 
 def des(input_block, subkeys, crypt_type):
-    initial_permutation_table = [
-        57, 49, 41, 33, 25, 17, 9, 1,
-        59, 51, 43, 35, 27, 19, 11, 3,
-        61, 53, 45, 37, 29, 21, 13, 5,
-        63, 55, 47, 39, 31, 23, 15, 7,
-        56, 48, 40, 32, 24, 16, 8, 0,
-        58, 50, 42, 34, 26, 18, 10, 2,
-        60, 52, 44, 36, 28, 20, 12, 4,
-        62, 54, 46, 38, 30, 22, 14, 6
-    ]
-    final_permutation_table = [
-        39, 7, 47, 15, 55, 23, 63, 31,
-        38, 6, 46, 14, 54, 22, 62, 30,
-        37, 5, 45, 13, 53, 21, 61, 29,
-        36, 4, 44, 12, 52, 20, 60, 28,
-        35, 3, 43, 11, 51, 19, 59, 27,
-        34, 2, 42, 10, 50, 18, 58, 26,
-        33, 1, 41, 9, 49, 17, 57, 25,
-        32, 0, 40, 8, 48, 16, 56, 24
-    ]
-
-    initial_permutation = permute(input_block, initial_permutation_table)
+    initial_permutation = permute(input_block, INITIAL_PERMUTATION_TABLE)
 
     print()
     print()
     fprint('BLOCK', input_block)
     fprint('INITIAL PERMUTATION', initial_permutation)
 
-    if crypt_type == 'e':
-        start = 0
-        stop = 16
-        step = 1
-    else:
-        start = 15
-        stop = -1
-        step = -1
-
+    rounds = range(16) if crypt_type == 'e' else reversed(range(16))
     output = initial_permutation
-    for i, j in enumerate(range(start, stop, step), 1):
+
+    for i, j in enumerate(rounds, 1):
         print()
         print(f'ROUND {i}:')
         output = round(output, subkeys[j])
 
     swap = output[BLOCK_SIZE // 2:] + output[:BLOCK_SIZE // 2]
-    final_permutation = permute(swap, final_permutation_table)
+    final_permutation = permute(swap, FINAL_PERMUTATION_TABLE)
 
     print()
     fprint('SWAP', swap)
@@ -228,63 +225,65 @@ def des(input_block, subkeys, crypt_type):
 
 
 def crypt(mode, crypt_type, key_file, infile, outfile, iv_file=None):
-    bin_in_str = pad(hex_to_bin(infile.read()[:-1]))
-    subkeys = gen_subkeys(hex_to_bin(key_file.read()[:-1]))
+    bin_in_str = pad(hex_to_bin(infile.read().strip()))
+    subkeys = gen_subkeys(hex_to_bin(key_file.read().strip()))
     bin_out_str = ''
 
     if mode == 'ecb':
         for i in range(0, len(bin_in_str), BLOCK_SIZE):
-            bin_out_str += des(bin_in_str[i:i + BLOCK_SIZE], subkeys, crypt_type)
-
+            block = bin_in_str[i:i + BLOCK_SIZE]
+            bin_out_str += des(block, subkeys, crypt_type)
     else:
-        last_block = hex_to_bin(iv_file.read()[:-1])
+        last_block = hex_to_bin(iv_file.read().strip())
 
         for i in range(0, len(bin_in_str), BLOCK_SIZE):
             block = bin_in_str[i:i + BLOCK_SIZE]
-
             if crypt_type == 'e':
                 block = xor(block, last_block)
-
             output = des(block, subkeys, crypt_type)
-
             if crypt_type == 'e':
                 last_block = output
-
             else:
                 output = xor(output, last_block)
                 last_block = block
-
             bin_out_str += output
 
     outfile.write(f'{int(bin_out_str, 2):0{len(bin_out_str) // 4}X}\n')
 
 
-if __name__ == '__main__':
-    def add_common_arguments(parser):
-        crypt_group = parser.add_mutually_exclusive_group(required='True')
-        crypt_group.add_argument('-e', action='store_const', dest='option', const='e', help='encrypt')
-        crypt_group.add_argument('-d', action='store_const', dest='option', const='d', help='decrypt')
-        parser.add_argument('key_file', type=argparse.FileType('r'),
-                            help='text file to be used as key for encryption/decryption')
-        parser.add_argument('infile', type=argparse.FileType('r'),
-                            help='text file to be used as input for encryption/decryption')
-        parser.add_argument('outfile', type=argparse.FileType('w'),
-                            help='text file to be used as output for encryption/decryption')
+def add_common_arguments(parser):
+    crypt_group = parser.add_mutually_exclusive_group(required=True)
+    crypt_group.add_argument('-e', action='store_const', dest='option', const='e', help='Encrypt the input file.')
+    crypt_group.add_argument('-d', action='store_const', dest='option', const='d', help='Decrypt the input file.')
 
-        return parser
+    parser.add_argument('key_file', type=argparse.FileType('r'),
+                        help='Path to the text file used as the key for encryption/decryption.')
+    parser.add_argument('infile', type=argparse.FileType('r'),
+                        help='Path to the text file used as input for encryption/decryption.')
+    parser.add_argument('outfile', type=argparse.FileType('w'),
+                        help='Path to the text file where the output will be written.')
+
+    return parser
 
 
+def main():
     parser = argparse.ArgumentParser()
-    sub_parsers = parser.add_subparsers(dest='mode')
+    sub_parsers = parser.add_subparsers(dest='mode', required=True)
+
     ecb_parser = add_common_arguments(
-        sub_parsers.add_parser('ecb', help='use Electronic Codebook (ECB) encryption mode'))
+        sub_parsers.add_parser('ecb', help='Use Electronic Codebook (ECB) encryption mode.'))
     cbc_parser = add_common_arguments(
-        sub_parsers.add_parser('cbc', help='use Cipher Block Chaining (CBC) encryption mode'))
+        sub_parsers.add_parser('cbc', help='Use Cipher Block Chaining (CBC) encryption mode.'))
     cbc_parser.add_argument('iv_file', type=argparse.FileType('r'),
-                            help='text file to be used as IV for encryption/decryption')
+                            help='Text file to be used as IV for encryption/decryption.')
+
     args = parser.parse_args()
 
     if args.mode == 'ecb':
         crypt(args.mode, args.option, args.key_file, args.infile, args.outfile)
     else:
         crypt(args.mode, args.option, args.key_file, args.infile, args.outfile, args.iv_file)
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_des.py
+++ tests/test_des.py
@@ -4,69 +4,85 @@ import unittest
 from des.des import hex_to_bin, pad, left_rotate, permute, gen_subkeys, xor, s_box, round, des, crypt
 
 
-class Test(unittest.TestCase):
+class TestDES(unittest.TestCase):
     def test_hex_to_bin(self):
-        self.assertEqual('0000000100100011010001010110011110001001101010111100110111101111',
-                         hex_to_bin('0123456789ABCDEF'))
+        self.assertEqual(hex_to_bin('0123456789ABCDEF'),
+                         '0000000100100011010001010110011110001001101010111100110111101111')
 
     def test_pad(self):
-        self.assertEqual(0, len(pad('')))
-        self.assertEqual(64, len(pad('0')))
-        self.assertEqual(64, len(pad('0' * 32)))
-        self.assertEqual(128, len(pad('0' * 128)))
+        self.assertEqual(len(pad('')), 0)
+        self.assertEqual(len(pad('0')), 64)
+        self.assertEqual(len(pad('0' * 32)), 64)
+        self.assertEqual(len(pad('0' * 128)), 128)
 
     def test_left_rotate(self):
-        self.assertEqual(['10' * 14], left_rotate(['01' * 14], 1))
+        self.assertEqual(left_rotate(['01' * 14], 1), ['10' * 14])
 
     def test_permute(self):
-        self.assertEqual('10' * 16, permute('01' * 16, [i for i in range(31, -1, -1)]))
+        self.assertEqual(permute('01' * 16, list(range(31, -1, -1))), '10' * 16)
 
     def test_gen_subkeys(self):
-        self.assertEqual(
-            ['101100001001001011001010110101010000001001010100', '101100000001101011010010110100011000001001010100',
-             '001101000111101001010000010100011010011010001100', '000001100111010101010100001110000011010010001101',
-             '010011100100010101010101001010100111000010100111', '010011111100000100101001001001100110100110100011',
-             '100010111000000110101011101001100000100101010011', '101110010000101010001011110001111000001101010010',
-             '001110010001101010001010110001011000001101001010', '001100000011100011001100010101001001011001001100',
-             '000100000110110001010100010110001001010011101100', '010001000110110100110100000010001111110010101001',
-             '110001101010010100100101001010100111110000110001', '110010111000011000100011101010110100100100110010',
-             '111010011001001010101010100001010100101100010010', '101000011001001010101010100101010000101101010000'],
-            gen_subkeys('01' * 32))
+        expected_keys = [
+            '101100001001001011001010110101010000001001010100',
+            '101100000001101011010010110100011000001001010100',
+            '001101000111101001010000010100011010011010001100',
+            '000001100111010101010100001110000011010010001101',
+            '010011100100010101010101001010100111000010100111',
+            '010011111100000100101001001001100110100110100011',
+            '100010111000000110101011101001100000100101010011',
+            '101110010000101010001011110001111000001101010010',
+            '001110010001101010001010110001011000001101001010',
+            '001100000011100011001100010101001001011001001100',
+            '000100000110110001010100010110001001010011101100',
+            '010001000110110100110100000010001111110010101001',
+            '110001101010010100100101001010100111110000110001',
+            '110010111000011000100011101010110100100100110010',
+            '111010011001001010101010100001010100101100010010',
+            '101000011001001010101010100101010000101101010000'
+        ]
+        self.assertEqual(gen_subkeys('01' * 32), expected_keys)
 
     def test_xor(self):
-        self.assertEqual('0' * 48, xor('01' * 24, '01' * 24))
+        self.assertEqual(xor('01' * 24, '01' * 24), '0' * 48)
 
     def test_s_box(self):
-        self.assertEqual('11000001010100101111110101010110', s_box('01' * 24))
+        self.assertEqual(s_box('01' * 24), '11000001010100101111110101010110')
 
     def test_round(self):
-        self.assertEqual('0101010101010101010101010101010101101101100011101010110010011110',
-                         round('01' * 32, '01' * 24))
+        self.assertEqual(round('01' * 32, '01' * 24),
+                         '0101010101010101010101010101010101101101100011101010110010011110')
 
     def test_des(self):
-        expected_results = ['0011111110111110101011100011110101101101001101110111011011001100',
-                            '0011111110111110101011100011110101101101001101110111011011001100']
+        expected_results = [
+            '0011111110111110101011100011110101101101001101110111011011001100',
+            '0011111110111110101011100011110101101101001101110111011011001100'
+        ]
 
         for i, option in enumerate(['e', 'd']):
-            self.assertEqual(expected_results[i], des('01' * 32, ['01' * 24] * 16, option))
+            self.assertEqual(des('01' * 32, ['01' * 24] * 16, option), expected_results[i])
 
     def test_crypt(self):
-        expected_results = ['DA02CE3A89ECAC3BDA02CE3A89ECAC3B', '411B8280950607EF411B8280950607EF',
-                            'E7BE36D4719EB0B790DF1619A4B7FAB9', 'BEE47D7F6AF9F810435D084E79AE63CF']
+        expected_results = [
+            'DA02CE3A89ECAC3BDA02CE3A89ECAC3B',
+            '411B8280950607EF411B8280950607EF',
+            'E7BE36D4719EB0B790DF1619A4B7FAB9',
+            'BEE47D7F6AF9F810435D084E79AE63CF'
+        ]
 
         for i, (mode, option) in enumerate(itertools.product(['ecb', 'cbc'], ['e', 'd'])):
             with self.subTest(mode=mode, option=option):
-                with open('tests/test_key_file.txt') as test_key_file:
-                    with open('tests/test_infile.txt') as test_infile:
-                        with open('tests/test_outfile.txt', 'w+') as test_outfile:
-                            if mode == 'cbc':
-                                with open('tests/test_iv_file.txt') as test_iv_file:
-                                    crypt(mode, option, test_key_file, test_infile, test_outfile, test_iv_file)
-                            else:
-                                crypt(mode, option, test_key_file, test_infile, test_outfile)
+                with open('tests/test_key_file.txt') as test_key_file, \
+                        open('tests/test_infile.txt') as test_infile, \
+                        open('tests/test_outfile.txt', 'w+') as test_outfile:
 
-                            test_outfile.seek(0)
-                            self.assertEqual(expected_results[i], test_outfile.read()[:-1])
+                    if mode == 'cbc':
+                        with open('tests/test_iv_file.txt') as test_iv_file:
+                            crypt(mode, option, test_key_file, test_infile, test_outfile, test_iv_file)
+                    else:
+                        crypt(mode, option, test_key_file, test_infile, test_outfile)
+
+                    test_outfile.seek(0)
+                    self.assertEqual(test_outfile.read().strip(), expected_results[i])
 
 
 if __name__ == "__main__":
